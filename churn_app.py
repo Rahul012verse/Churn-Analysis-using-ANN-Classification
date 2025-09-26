7d1749dfd118436e0349187f21d5576723acbf72
Rahul012verse <153096245+Rahul012verse@users.noreply.github.com>
2025-09-26 11:51:44 +0530

    churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -1,9 +1,9 @@
+import tensorflow
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import numpy as np
 import pickle
 import streamlit as st
-import tensorflow
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
 churn_model = tensorflow.keras.models.load_model("churn_model.h5")
 
@@ -62,6 +62,7 @@ if prediction > 0.5:
     st.write('The customer is likely to churn.')
 else:
     st.write('The customer is not likely to churn.')
+
 
 
 
